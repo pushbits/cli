49e80649056dc2cae75ca9bbb6b39a754423c35d
eikendev <raphael@eiken.dev>
2021-02-09 00:22:02 +0100

    Implement partial update command for users

--- internal/user/mod.go
+++ internal/user/mod.go
@@ -4,6 +4,7 @@ package user
 type Command struct {
 	Create createCommand `command:"create" alias:"c" description:"Create a new user"`
 	Delete deleteCommand `command:"delete" alias:"d" description:"Delete an existing user"`
+	Update updateCommand `command:"update" alias:"u" description:"Update an existing user"`
 	List   listCommand   `command:"list" alias:"l" description:"List all existing users"`
 	Show   showCommand   `command:"show" alias:"s" description:"Show details of an existing user"`
 }
---
+++ internal/user/update.go
@@ -0,0 +1,48 @@
+package user
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/pushbits/cli/internal/api"
+	"github.com/pushbits/cli/internal/settings"
+	"github.com/pushbits/cli/internal/ui"
+)
+
+const (
+	updateEndpoint = "/user/%d"
+)
+
+type updateCommand struct {
+	Arguments struct {
+		ID uint `positional-arg-name:"id" description:"The ID of the user"`
+	} `required:"true" positional-args:"true"`
+	NewName     *string `long:"new-name" description:"The new name of the user"`
+	NewMatrixID *string `long:"new-matrixid" description:"The new Matrix ID of the user"`
+}
+
+func (c *updateCommand) Execute(args []string) error {
+	settings.Runner = c
+	return nil
+}
+
+func (c *updateCommand) Run(s settings.Settings, password string) {
+	data := map[string]interface{}{}
+
+	if c.NewName != nil {
+		data["name"] = c.NewName
+	}
+
+	if c.NewMatrixID != nil {
+		data["matrix_id"] = c.NewMatrixID
+	}
+
+	populatedEndpoint := fmt.Sprintf(updateEndpoint, c.Arguments.ID)
+
+	resp, err := api.Put(s.URL, populatedEndpoint, s.Username, password, data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ui.PrintJSON(resp)
+}
